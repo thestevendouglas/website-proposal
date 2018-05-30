f7719ed3823db5c12aec946fa03c15345b370664
Steven Antonsen <stevenantonsen@Stevens-MacBook-Pro.local>
2018-05-30 15:10:49 -0700

    week-8 homework

---
+++ advisory-board.js
@@ -0,0 +1,25 @@
+//var name = ['J.L. Martinez', 'Ben Amaba, PhD', 'Brian Yablonski', 'Byron A. Knibbs', 'Christopher Fountas', 'Dan Holladay', 'Gustavo R. Cepero', 'Jennifer Grove', 'Jeremy L. Susac', 'Nick Gladding', 'Paul Zombe P.E.', 'Richard Paul-Hus', 'Roy Periana', 'Thomas L. Hernandez', 'Timothy L. Bryant']
+
+
+
+
+//var advisoryBoard = new Vue ({
+ // el: '#advisory-board',
+ // data: {
+  //  boardMembers: name 
+//  }
+  
+//})
+
+var app = new Vue({
+  el: '#app-header',
+  data: { }
+});
+
+var footerApp = new Vue ({
+  el: '#app-footer',
+  data:{
+    
+    
+  }
+})
---
+++ script.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: '#app-header',
+  data: { }
+});
+
+var footerApp = new Vue ({
+  el: '#app-footer',
+  data:{
+    
+    
+  }
+})
